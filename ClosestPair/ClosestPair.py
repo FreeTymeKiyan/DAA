2a16bf22ff2c2b2c9c70f979b5357f548cbb35e1
FreeTymeKiyan <FreeTymeSunKiyan@gmail.com>
2013-09-29 18:29:24 -0400

    initial commit

---
+++ ClosestPair/ClosestPair.py
@@ -0,0 +1,9 @@
+# python
+# implement a divide and conquer closest pair algorithm
+# 1. check read and parse .txt file
+# 2. use the parsed result as the input
+# 3. find the closest pair in these input points
+# 4. output the closest pair and the distance between the pair
+#	output format
+#	The closest pair is (x1, x2) and (y1, y2).
+#	The distance between them is d = xxx.
